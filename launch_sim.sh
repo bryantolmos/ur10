adb94ccf7e7d54373a3a19d2c7fef384d756a28d
bryantolmos <olmosbryant@outlook.com>
2026-04-15 00:01:33 -0700

    Fixed Gazebo mesh loading and controller spawning
    - Updated "launch_sim.sh" to dynamically resolve package prefix paths using "ros2 pkg prefix" ensuring Gazebo locates the "ur_description" collision STLs

--- launch_sim.sh
+++ launch_sim.sh
@@ -9,9 +9,13 @@ source /opt/ros/jazzy/setup.bash
 # 3. Source the local workspace overlay
 source ${WS_DIR}/install/setup.bash
 
-# 4. Explicitly set the Gazebo paths.
-export GZ_SIM_SYSTEM_PLUGIN_PATH=${WS_DIR}/install/lib:/opt/ros/jazzy/lib
-export GZ_SIM_RESOURCE_PATH=${WS_DIR}/src/:/opt/ros/jazzy/share
+# 4. Dynamically find the exact package locations
+UR_SHARE=$(ros2 pkg prefix ur_description)/share
+CUSTOM_SHARE=$(ros2 pkg prefix ur10_description)/share
 
-# 5. Launch the simulation
+# 5. Export Gazebo Paths
+export GZ_SIM_SYSTEM_PLUGIN_PATH=${WS_DIR}/install/lib:/opt/ros/jazzy/lib
+export GZ_SIM_RESOURCE_PATH=${UR_SHARE}:${CUSTOM_SHARE}:${WS_DIR}/src:/opt/ros/jazzy/share
+
+# 6. Launch the simulation
 ros2 launch ur10_moveit_config ur10_gazebo_moveit.launch.py
